2f770dda7d9de19f60626e61edf267609d6103a6
Benoit-Hayet <hayetbenoit@gmail.com>
2024-07-15 13:09:06 +0200

    first commit

---
+++ src/main/java/org/myblognew/MyBlogNew/MyBlogNewApplication.java
@@ -0,0 +1,13 @@
+package org.myblognew.MyBlogNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/myblognew/MyBlogNew/MyBlogNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myblognew.MyBlogNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
