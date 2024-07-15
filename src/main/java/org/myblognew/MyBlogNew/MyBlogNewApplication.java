b679e0b688ac8e9243e8a0d7d942723f5f5465f9
Benoit-Hayet <hayetbenoit@gmail.com>
2024-07-15 13:54:59 +0200

    New Branch 02

--- src/main/java/org/myblognew/MyBlogNew/MyBlogNewApplication.java
+++ src/main/java/org/myblognew/MyBlogNew/MyBlogNewApplication.java
@@ -2,12 +2,16 @@ package org.myblognew.MyBlogNew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+
 public class MyBlogNewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogNewApplication.class, args);
 	}
 
-}
+	}
