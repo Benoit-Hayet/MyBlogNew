4dc0280d02373d0bad6cd879fd38daafddfa76db
Benoit-Hayet <hayetbenoit@gmail.com>
2024-07-18 13:50:15 +0200

    All Category CRUD Ok

--- src/main/java/org/myblognew/MyBlogNew/controller/CategoryController.java
+++ src/main/java/org/myblognew/MyBlogNew/controller/CategoryController.java
@@ -3,11 +3,9 @@ package org.myblognew.MyBlogNew.controller;
 
 import org.myblognew.MyBlogNew.model.Article;
 import org.myblognew.MyBlogNew.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.myblognew.MyBlogNew.repository.CategoryRepository;
 
 import java.util.List;
@@ -42,6 +40,30 @@ public class CategoryController {
 
         Category category = optionalCategory.get();
         return ResponseEntity.ok(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        category.setName(categoryDetails.getName());
+        Category updatedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        categoryRepository.delete(category);
+        return ResponseEntity.noContent().build();
     }
 }
 
